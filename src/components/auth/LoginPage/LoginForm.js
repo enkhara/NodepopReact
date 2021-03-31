10fa26cb9739164f5adc16170d23c7fdc1e1fdf8
Georgina Torralba <georgina.torralba@gmail.com>
2021-04-01 00:38:53 +0200

    Credentials controladas en el form

--- src/components/auth/LoginPage/LoginForm.js
+++ src/components/auth/LoginPage/LoginForm.js
@@ -4,6 +4,18 @@ import FormField from '../../shared/FormField';
 import Button from '../../shared/Button';
 
 function LoginForm() {
+	const [credentials, setCredentials] = React.useState({
+		username: '',
+		password: '',
+	});
+
+	const handleChange = (event) => {
+		setCredentials((oldCredentials) => ({
+			...oldCredentials,
+			[event.target.name]: event.target.value,
+		}));
+	};
+
 	return (
 		<form className="loginForm">
 			<FormField
@@ -11,18 +23,22 @@ function LoginForm() {
 				type="text"
 				label="Email"
 				className="loginForm-field"
+				value={credentials.username}
+				onChange={handleChange}
 			/>
 			<FormField
 				name="password"
 				type="password"
 				label="password"
 				className="loginForm-field"
+				value={credentials.password}
+				onChange={handleChange}
 			/>
 			<Button
 				type="submit"
-				//variant="primary"
+				variant="primary"
 				className="loginForm-submit"
-				// disabled={isLoading || !username || !password}
+				//disabled={isLoading || !username || !password}
 			>
 				Log in
 			</Button>
