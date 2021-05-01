8b84163cd54c72750b2841cdf58d32d39a9795a6
Georgina Torralba <georgina.torralba@gmail.com>
2021-05-01 13:27:02 +0200

    Destructuring credentials

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,10 @@ import client, { configureClient, resetClient } from './client';
 
 import storage from '../utils/storage';
 
-export const login = (credentials) => {
+export const login = (allCredentials) => {
+	const { remind, ...credentials } = allCredentials;
+	console.log('credentials', credentials);
+	console.log('remind', remind);
 	return client.post('/api/auth/login', credentials).then(({ accessToken }) => {
 		configureClient({ accessToken });
 		if (credentials.remind) {
