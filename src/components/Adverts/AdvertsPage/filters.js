66fb2ef2b5e9f6a1cd32801b620b589a29b2a9df
Georgina Torralba <georgina.torralba@gmail.com>
2021-06-24 21:13:50 +0200

    delete comments

--- src/components/Adverts/AdvertsPage/filters.js
+++ src/components/Adverts/AdvertsPage/filters.js
@@ -7,46 +7,6 @@ export const initialFilter = {
 	//tags: [],
 };
 
-// export const filterAdverts = (adverts, filterAdvert, tags) => {
-// 	return adverts
-// 		.filter(
-// 			(advert) =>
-// 				!filterAdvert.advertName ||
-// 				filterAdvert.advertName === advert.name.toLowerCase()
-// 		)
-// 		.filter(
-// 			(advert) =>
-// 				(!filterAdvert.buy && !filterAdvert.sale) ||
-// 				(filterAdvert.buy && filterAdvert.sale) ||
-// 				filterAdvert.buy === !advert.sale ||
-// 				filterAdvert.sale === advert.sale
-// 		)
-// 		.filter(
-// 			(advert) =>
-// 				(!filterAdvert.maxPrice && !filterAdvert.minPrice) ||
-// 				filterAdvert.maxPrice < filterAdvert.minPrice ||
-// 				(advert.price <= filterAdvert.maxPrice &&
-// 					advert.price >= filterAdvert.minPrice)
-// 		)
-// 		.filter(
-// 			//(advert) =>	!tags.length || advert.tags.every((tags) => tags.includes(tags))
-// 			(advert) => !tags.length || advert.tags.every((tag) => tags.includes(tag))
-// 			// (tags.length === 1 && advert.tags.find((tag) => tag === tags[0])) ||
-// 			// (tags.length === 2 &&
-// 			// 	advert.tags.find((tag) => tag === tags[0]) &&
-// 			// 	advert.tags.find((tag) => tag === tags[1])) ||
-// 			// (tags.length === 3 &&
-// 			// 	advert.tags.find((tag) => tag === tags[0]) &&
-// 			// 	advert.tags.find((tag) => tag === tags[1]) &&
-// 			// 	advert.tags.find((tag) => tag === tags[2])) ||
-// 			// (tags.length === 4 &&
-// 			// 	advert.tags.find((tag) => tag === tags[0]) &&
-// 			// 	advert.tags.find((tag) => tag === tags[1]) &&
-// 			// 	advert.tags.find((tag) => tag === tags[2]) &&
-// 			// 	advert.tags.find((tag) => tag === tags[3]))
-// 		);
-// };
-
 export const filterAdverts = (adverts, filterAdvert, tags) => {
 	return adverts
 		.filter(
